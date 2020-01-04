850f7635af7a57e1a3c1002bbf2fb6ee3bf9983b
ewenaiwe <ewenaiwe@gmail.com>
2020-01-04 16:44:20 +0000

    Done.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -7,6 +7,6 @@ class SightingsController < ApplicationController
 
     def show
         sighting = Sighting.find(params[:id])
-        render json: SightingSerializer.new(sighting)to_serialized_json
+        render json: SightingSerializer.new(sighting).to_serialized_json
     end
 end
